88d98f27dccb041345deb9a3830fe54909af724e
marcell ciszek <ciszekmarcell@gmail.com>
2025-06-20 17:21:17 +0200

    feat: add core functionality for tailwind-merge
    - Implemented `fromTheme` function to retrieve theme values.
    - Created an LRU cache implementation for efficient caching.
    - Developed `mergeClassList` to handle merging of Tailwind class lists.
    - Added `mergeConfigs` to facilitate configuration merging.
    - Introduced `parseClassName` for parsing Tailwind class names.
    - Implemented `sortModifiers` to sort class modifiers correctly.
    - Created `twJoin` for joining class names.
    - Developed `twMerge` as the main export for merging Tailwind classes.
    - Defined types for configuration and class groups.
    - Added validators for various Tailwind class formats.
    - Updated dependencies for clsx and tailwind-merge.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,17 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import "./App.css"
-import { TaskApp } from "./components/task-app"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import "./App.css";
+import {TaskApp} from "./components/task-app";
 
-let queryClient = new QueryClient()
-
-
+let queryClient = new QueryClient();
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TaskApp />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<TaskApp />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
 
-
-
-export default App
+export default App;
--- client/src/components/task-app.tsx
+++ client/src/components/task-app.tsx
@@ -1,7 +1,31 @@
-interface Props  {
- 
+import {useQuery} from "@tanstack/react-query";
+
+const baseUrl = "http://0.0.0.0:8080/tasks/api/v1";
+async function fetchTodos() {
+	try {
+		let response = await fetch(baseUrl);
+		let data = await response.json();
+		return data;
+	} catch (error) {
+		// biome-ignore lint/suspicious/noConsole: <explanation>
+		console.error("Error fetching todos:", error);
+		throw error;
+	}
 }
 
-export function TaskApp  ({}:Props)  {
-  return <h1> TaskApp   </h1>
+export function TaskApp() {
+	let {isLoading, isError, data, error} = useQuery({
+		queryKey: ["todos"],
+		queryFn: fetchTodos,
+	});
+	if (isLoading) return <h1>Loading...</h1>;
+	if (isError) return <h1>Error: {error?.message ?? "Ooops!"}</h1>;
+
+	console.log(data);
+
+	return (
+		<div className="border border-red-500 p-10">
+			<h1> TaskApp </h1>
+		</div>
+	);
 }
