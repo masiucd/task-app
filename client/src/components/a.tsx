8ee8400167774342dabeb8110d7225389a54a3bf
marcell ciszek <ciszekmarcell@gmail.com>
2025-06-22 15:01:01 +0200

    feat: add MantineLinkComponent for enhanced anchor functionality

---
+++ client/src/components/a.tsx
@@ -0,0 +1,19 @@
+import {Anchor, type AnchorProps} from "@mantine/core";
+import {type LinkComponent, createLink} from "@tanstack/react-router";
+import * as React from "react";
+
+interface MantineAnchorProps extends Omit<AnchorProps, "href"> {
+	// Add any additional props you want to pass to the anchor
+}
+
+const MantineLinkComponent = React.forwardRef<HTMLAnchorElement, MantineAnchorProps>(
+	(props, ref) => {
+		return <Anchor ref={ref} {...props} />;
+	},
+);
+
+const CreatedLinkComponent = createLink(MantineLinkComponent);
+
+export const A: LinkComponent<typeof MantineLinkComponent> = (props) => {
+	return <CreatedLinkComponent preload="intent" {...props} />;
+};
